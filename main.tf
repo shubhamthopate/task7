283c55c8a6cbfc223c0e1443ff990d1cfc693f84
shubhamthopate <shubhamthopate9960@gmail.com>
2022-01-26 10:05:36 +0530

    safas

--- main.tf
+++ main.tf
@@ -7,5 +7,12 @@ provider "azurerm" {
 
 module "module1" {
  source = "./modules/module1"
+ ip_names = var.ip_names
+ sku_list = var.sku_list
+ sku_tier = var.sku_tier
+ allocation_method = var.allocation_method
+ az = var.az
+ ip_version = var.ip_version
+ idle_timeout = var.idle_timeout
  
 }
