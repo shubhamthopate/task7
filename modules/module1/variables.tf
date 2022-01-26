7b80d7c81148745a10152143a63b1b05ae85a776
shubhamthopate <shubhamthopate9960@gmail.com>
2022-01-26 10:00:14 +0530

    sdfsdf

--- modules/module1/variables.tf
+++ modules/module1/variables.tf
@@ -3,7 +3,7 @@
 variable "ip_names" {
     description = "ip names list"
     type = list(string)
-    default = ["pip_ads_dev_001","pip_ads_dev_002","pip_ads_dev_003","pip_ads_dev_004"]
+    
  
 }
 
@@ -12,36 +12,36 @@ variable "ip_names" {
 variable "sku_list" {
     description = "sku list"
     type = string
-    default = "Standard"
+ 
 }
 
 variable "sku_tier" {
     description = "sku tier"
     type = string
-    default = "Regional"
+
 }
 
 variable "allocation_method" {
     description = "allocation method"
     type = string
-    default = "Static"
+  
 }
 
 variable "az" {
     description = "avaialability zones"
     
-    default = ["1","2","Zone-Redundant","No-Zone"]
+
 
 }
 
 variable "ip_version" {
     description = "ip version"
     
-    default = ["IPv4","IPv4","IPv6","IPv4"]
+    
 
 }
 
 variable "idle_timeout" {
     description = "idle timeout"
-    default = ["5","21","12","25"]
+
 }
---
+++ terraform.tfvars
@@ -0,0 +1,7 @@
+ip_names = ["pip_ads_dev_001","pip_ads_dev_002","pip_ads_dev_003","pip_ads_dev_004"]
+sku_list = "Standard"
+sku_tier = "Regional"
+allocation_method = "Static"
+az = ["1","2","Zone-Redundant","No-Zone"]
+ip_version = ["IPv4","IPv4","IPv6","IPv4"]
+idle_timeout = ["5","21","12","25"]
